7a503baf0b5709c7bca6ed698ffe0c9eb5d67173
Ajusa Jain <arhamjain@gmail.com>
2016-01-29 22:09:32 -0500

    Added localStorage lib

---
+++ www/js/settingManager.js
@@ -0,0 +1,10 @@
+/*function Manager() {
+    riot.observable(this);
+    self = this;
+    self.settings;
+    self.on('debug', function(value) {    
+    	self.settings.debug = value;
+    	Lockr.set('settings', self.settings);
+    });
+}
+*/
